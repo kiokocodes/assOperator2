264769be313998d235ba0eb8f364da58a84f3a04
Joseph Kioko <kiokocodes@gmail.com>
2023-04-20 03:32:42 +0300

    assOp2 initializes a value the other funkie.html and funkie.js are just random thought detours about a function()

---
+++ assOp2.js
@@ -0,0 +1,31 @@
+// Initializing Variables with the Assignment Operator
+
+var a = 9;
+
+// initialization of a variable and assigning it a value within the same line
+
+
+/*
+
+The let keyword declares a block-scoped variable that can be reassigned a new value later in the code. 
+
+The const keyword declares a block-scoped variable that cannot be reassigned a new value once it is initialized. 
+
+The var keyword declares a variable with function scope (or global scope if declared outside a function), 
+and the variable can be reassigned a new value later in the code.
+
+
+
+In addition to these keywords, you can also declare a variable without an initial value using any of these keywords. For example:
+
+// Declare a variable called a using let, but do not initialize it with a value
+let a;
+
+// Declare a constant called b without initializing it with a value
+const b;
+
+// Declare a variable called c using var, but do not initialize it with a value
+var c;
+
+
+*/
---
+++ funkie.js
@@ -0,0 +1,5 @@
+
+function funkieText() {
+const element = document.getElementById("oldText");
+element.textContent = "Changed Text";
+}
